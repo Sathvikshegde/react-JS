cb818b9d1988458e64372dbde733b163483f3591
SATHVIK S HEGDE <155740540+Sathvikshegde@users.noreply.github.com>
2025-07-15 03:39:59 +0000

    bgchanger

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
   style={{ backgroundColor: "red" }}
 >
-  Red
+  Red 
 </button>
 <button onClick={()=>setcolor("green")}
   className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
